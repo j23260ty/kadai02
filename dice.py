78f251fce1f1da4e6c2eda89cf3a06125808a2b2
j23260竹内雪翔 <j23260ty@edu.tuis.ac.jp>
2025-04-21 11:06:10 +0900

    3,8,10,11行目を追加

--- dice.py
+++ dice.py
@@ -1,8 +1,14 @@
 import random
 
+total = 0
+
 for i in range(10):
     x = random.randint(1,6)
     print(str(i+1)+"回目："+str(x))
+    total += x
+    
+average =  total/ 10 
+print(average)
 
 # 期待される出力結果例
 """
